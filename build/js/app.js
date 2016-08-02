f77c4bab9991cad1521b7eee5d006d8f7de71e9b
Bhawani Parajuli and Caleb Stokka <bhawani+caleb@>
2016-08-02 14:40:19 -0700

    add new api function

--- build/js/app.js
+++ build/js/app.js
@@ -1,5 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var apiKey = "ce866ab8b564a5a186bed6c157235544";
+exports.apiKey = "ce866ab8b564a5a186bed6c157235544";
+
+},{}],2:[function(require,module,exports){
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
@@ -7,10 +10,16 @@ $(document).ready(function() {
     $('#location').val("");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
       $('#showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-      console.log(response);
-      console.log(JSON.stringify(response));
+    });
+  });
+  $("#cityId").click(function() {
+    var cityId = $('#city').val();
+    $('#city').val("");
+    $.get('http://api.openweathermap.org/data/2.5/forecast/daily?id=' + cityId + '&cnt=5' + '&appid=' + apiKey, function(forecast) {
+      $('#moWeather').text("The high tomorrow in " + forecast.city.name + " is " + ((forecast.list[1].temp.max) - 273.15) + " degrees Celsius.");
+      console.log(forecast);
     });
   });
 });
 
-},{}]},{},[1]);
+},{"./../.env":1}]},{},[2]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,4 +1,4 @@
-var apiKey = "ce866ab8b564a5a186bed6c157235544";
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
@@ -6,8 +6,14 @@ $(document).ready(function() {
     $('#location').val("");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
       $('#showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-      console.log(response);
-      console.log(JSON.stringify(response));
+    });
+  });
+  $("#cityId").click(function() {
+    var cityId = $('#city').val();
+    $('#city').val("");
+    $.get('http://api.openweathermap.org/data/2.5/forecast/daily?id=' + cityId + '&cnt=5' + '&appid=' + apiKey, function(forecast) {
+      $('#moWeather').text("The high tomorrow in " + forecast.city.name + " is " + ((forecast.list[1].temp.max) - 273.15) + " degrees Celsius.");
+      console.log(forecast);
     });
   });
 });
