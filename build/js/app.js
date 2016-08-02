aa9ac5ea3e318cfba4e21a41cb392b6d929f91e5
Bhawani Parajuli and Caleb Stokka <bhawani@>
2016-08-02 13:50:15 -0700

    ajax request returning json

---
+++ build/js/app.js
@@ -0,0 +1,16 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var apiKey = "ce866ab8b564a5a186bed6c157235544";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('#showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+      console.log(response);
+      console.log(JSON.stringify(response));
+    });
+  });
+});
+
+},{}]},{},[1]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -2,12 +2,12 @@ var apiKey = "ce866ab8b564a5a186bed6c157235544";
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    
     var city = $('#location').val();
     $('#location').val("");
-    $('#showWeather').text("The city you have chosen is " + city ".");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('#showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
       console.log(response);
+      console.log(JSON.stringify(response));
     });
   });
 });
